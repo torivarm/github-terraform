48957a61a5afad26e746fd37818476c0d005fda2
Tor Ivar Melling <tor.i.melling@ntnu.no>
2023-09-27 15:19:11 +0200

    var default value

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "rg_name" {
     type = string
     description = "Name of the resource group"
+    default = "rg-terraform"
 }
 
 variable "rg_location" {
     type = string
     description = "Location of the resource group"
+    default = "westeurope"
 }
